158d56c1e5f05ad37643fe1eb65c0377fdacc087
alexwillemen <50456347+alexwillemen@users.noreply.github.com>
2019-05-22 08:49:09 +0200

    Add files via upload

---
+++ Apotheek+last version/Prescriptions.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <title>Apotheek+</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    </head>
+
+    <body>
+        <div class="navbar">
+            <a class="bold" href="homepage.php">Apotheek+</a>
+            <div class="dropdown">
+                <button class="dropbtn">☰
+                <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                <a href="MedicalDocuments.php">Medical Documents</a>
+                <a href="Prescriptions.php">Prescriptions</a>
+                <a href="AskYourDoctor.php">Ask your doctor</a>
+                </div>
+            </div> 
+            <a href="login.php" class="testright">Account</a>
+            </div>
+
+        <h1>Prescriptions</h1>
+
+        <div class="names">
+            <form name="form2" action="" method="post">
+                <p class="small">Concept by: Alex Willemen - Taivan Enkhbayar - Tim Philippe - Jochen Snoeks - Luca De Boeck - Serhat</p>
+                <p class="small">Inspiration lab project: Chronic Illness Prescription Application</p>
+            </form>
+            </div>
+    </body>
+</html>
